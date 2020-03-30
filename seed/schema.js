c78b72a1b4aa965456d15d57710f347490c09b6e
Aldi Doanta <aldidkurnia@gmail.com>
2020-03-30 10:14:14 +0700

    [Seed] Add initial script to export firestore collection structure

---
+++ seed/schema.js
@@ -0,0 +1,25 @@
+exports.schema = {
+	articles: {},
+	articles_national: {},
+	articles_world: {},
+	banners: {},
+	broadcasts: {},
+	call_centers: {},
+	faqs: {},
+	infographics: {},
+	statistics: {
+		'jabar-dan-nasional': {
+			aktif: {},
+			sembuh: {},
+			meninggal: {},
+			odp: {
+				selesai: {},
+				total: {}
+			},
+			pdp: {
+				selesai: {},
+				total: {}
+			}
+		}
+	}
+};
