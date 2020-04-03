7e58a9979df370d31322ae792e36dc68da62bf02
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-04-03 14:06:11 +0700

    chore: update wording

--- pages/temp/result/_id.vue
+++ pages/temp/result/_id.vue
@@ -54,34 +54,43 @@ export default {
         </p>
         <br>
         <p>
-          Terima kasih telah memenuhi panggilan Tes Masif COVID-19 di Gereja Bethel Indonesia (GBI), Jalan Aruna No. 19, Bandung, pada 30 Maret - 1 April 2020.
+          Terima kasih telah memenuhi panggilan Tes Masif COVID-19.
         </p>
         <br>
         <p>
           Hasil tes menunjukkan bahwa Anda <b class="underline">POSITIF COVID-19</b>. </p>
         <br>
         <p>
-          Tetap tenang dan optimis bahwa Anda bisa melawan virus ini. Sebagai tindak lanjut, <b class="underline">Anda akan diundang untuk tes swab lanjutan</b>.
-          Waktu dan tempat akan segera kami informasikan kepada Anda.
+          Tetap tenang dan optimis bahwa Anda bisa melawan virus ini.
+          Sebagai tindak lanjut, <b>Anda akan diundang untuk tes swab lanjutan</b>.
+          Waktu dan tempat akan segera kami informasikan kepada Anda melalui SMS.
           Sementara ini, mohon perhatikan langkah-langkah berikut:
           <ol>
             <li>
-              Isolasi diri Anda. Jaga jarak dan hindari kontak fisik langsung dengan orang lain, termasuk keluarga.
+              <b>Isolasi diri Anda</b>. Jaga jarak dan hindari kontak fisik langsung dengan orang lain, termasuk keluarga seperti berjabat tangan.
+              Gunakan peralatan makan dan mandi sendiri, tidak bercampur dengan orang lain. 
             </li>
             <li>
-              Lakukan pola hidup bersih dan sehat
+              <b>Menggunakan masker bila sakit</b> atau harus berada di tempat umum.
             </li>
             <li>
-              Tetap berpikir positif, pikiran yang berbahagia akan membantu tubuhmu tetap kuat dan menjaga imun agar tidak jatuh sakit
+              Lakukan <b>pola hidup bersih dan sehat</b>, seperti rajin mencuci tangan pakai sabun dengan air mengalir sebelum menyentuh muka dan makan,
+              olahraga secara rutin, konsumsi multivitamin, istirahat yang cukup, dan mengurangi konsumsi gula.
             </li>
             <li>
-              Tingkatkan imun tubuh dengan mengkonsumsi banyak makanan bergizi
+              Tetap <b>berpikir positif</b>, pikiran yang bahagia akan membantu tubuhmu tetap kuat dan menjaga imun agar tidak jatuh sakit.
+            </li>
+            <li>
+              Tingkatkan <b>imun tubuh</b> dengan mengkonsumsi banyak makanan bergizi.
             </li>
           </ol>
+          <img class="object-contain object-center w-full h-auto" src="https://firebasestorage.googleapis.com/v0/b/jabarprov-covid19.appspot.com/o/public%2Frdt-pikobar.png?alt=media&token=00c5a8c1-de59-4657-8097-6e84ba0195b6">
         </p>
-        <br>
         <p>
-          Untuk keadaan darurat Anda bisa menghubungi
+          Observasi gejala yang Anda alami. Untuk positif tanpa gejala atau dengan gejala ringan sampai sedang,
+          Anda disarankan untuk melakukan isolasi dan perawatan mandiri.
+          Apabila gejala berlanjut, segera konsultasi ke dokter atau fasyankes terdekat.
+          Untuk pertanyaan lebih lanjut, Anda dapat menghubungi:
           <br><br>
           Call Center: <a class="temp-result-call-button" href="tel:119">119</a>
           <br>
@@ -94,7 +103,7 @@ export default {
         </p>
         <br>
         <p>
-          Terima kasih telah memenuhi panggilan Tes Masif COVID-19 di Gereja Bethel Indonesia (GBI), Jalan Aruna No. 19, Bandung, pada 30 Maret - 1 April 2020.
+          Terima kasih telah memenuhi panggilan Tes Masif COVID-19.
         </p>
         <br>
         <p>
@@ -102,23 +111,26 @@ export default {
         </p>
         <br>
         <p>
-          Walaupun demikian, <b class="underline">kami sarankan Anda tetap berkegiatan dari rumah selama pandemi berlangsung</b>.
-          Kami berharap Anda senantiasa menerapkan pembatasan sosial untuk bantu cegah penyebaran COVID-19, dengan langkah-langkah berikut:
+          Walaupun demikian, bukan berarti Anda kebal dari virus COVID-19.
+          <b>Kami meminta Anda tetap berkegiatan dari rumah selama pandemi berlangsung</b>.
+          Anda diwajibkan senantiasa menerapkan pembatasan sosial untuk bantu cegah penyebaran COVID-19, dengan langkah-langkah berikut:
           <ol>
             <li>
-              Hindari bepergian dan tetap berkegiatan dari rumah serta tidak mudik untuk sementara waktu
+              Hindari bepergian dan tetap <b>berkegiatan dari rumah</b> serta <b>tidak mudik</b> untuk sementara waktu
             </li>
             <li>
-              Lakukan pola hidup bersih dan sehat, seperti rajin mencuci tangan, olahraga, makan-makanan bergizi dan minum multivitamin untuk menjaga imun tubuh
+              Lakukan <b>pola hidup bersih dan sehat</b>,
+              seperti rajin mencuci tangan pakai sabun dengan air mengalir sebelum menyentuh muka dan makan,
+              olahraga secara rutin, konsumsi multivitamin, istirahat yang cukup, dan mengurangi konsumsi gula.
             </li>
             <li>
-              Tidak melakukan <i>panic buying</i> terhadap alat-alat kebutuhan medis seperti masker, karena tenaga medis dan orang yang sakit lebih membutuhkan.
+              Tidak melakukan <b><i>panic buying</i> terhadap alat kebutuhan sehari-hari dan kebutuhan medis</b> seperti masker, karena tenaga medis dan orang yang sakit lebih membutuhkan. 
             </li>
           </ol>
+          <img class="object-contain object-center w-full h-auto" src="https://firebasestorage.googleapis.com/v0/b/jabarprov-covid19.appspot.com/o/public%2Frdt-pikobar.png?alt=media&token=00c5a8c1-de59-4657-8097-6e84ba0195b6">
         </p>
-        <br>
         <p>
-          Untuk keadaan darurat Anda bisa menghubungi
+          Untuk pertanyaan lebih lanjut, Anda bisa menghubungi:
           <br><br>
           Call Center: <a class="temp-result-call-button" href="tel:119">119</a>
           <br>
