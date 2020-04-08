a9bf8633eb9d52d7fee349104f87436b8dea94ad
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-04-09 00:49:08 +0700

    feat: add numerical data formatter

--- components/_pages/index/DataSummary.vue
+++ components/_pages/index/DataSummary.vue
@@ -13,7 +13,7 @@
               <b>Jawa Barat</b>
             </h4>
             <b>
-              {{ positifJabar }}
+              {{ formatNumber(positifJabar) }}
             </b>
           </div>
           <div class="flex justify-between items-baseline text-xl opacity-50">
@@ -21,7 +21,7 @@
               Indonesia
             </h4>
             <b>
-              {{ positifNasional }}
+              {{ formatNumber(positifNasional) }}
             </b>
           </div>
         </CounterCardLoader>
@@ -36,7 +36,7 @@
               <b>Jawa Barat</b>
             </h4>
             <b>
-              {{ sembuhJabar }}
+              {{ formatNumber(sembuhJabar) }}
             </b>
           </div>
           <div class="flex justify-between items-baseline text-xl opacity-50">
@@ -44,7 +44,7 @@
               Indonesia
             </h4>
             <b>
-              {{ sembuhNasional }}
+              {{ formatNumber(sembuhNasional) }}
             </b>
           </div>
         </CounterCardLoader>
@@ -59,7 +59,7 @@
               <b>Jawa Barat</b>
             </h4>
             <b>
-              {{ meninggalJabar }}
+              {{ formatNumber(meninggalJabar) }}
             </b>
           </div>
           <div class="flex justify-between items-baseline text-xl opacity-50">
@@ -67,7 +67,7 @@
               Indonesia
             </h4>
             <b>
-              {{ meninggalNasional }}
+              {{ formatNumber(meninggalNasional) }}
             </b>
           </div>
         </CounterCardLoader>
@@ -132,6 +132,7 @@ import _get from 'lodash/get'
 import _round from 'lodash/round'
 import CounterCardLoader from './CounterCardLoader'
 import StatisticLoader from './StatisticLoader'
+import { formatNumber } from '~/lib/number'
 
 export default {
   components: {
@@ -183,7 +184,8 @@ export default {
     }
   },
   methods: {
-    _round
+    _round,
+    formatNumber
   }
 }
 </script>
