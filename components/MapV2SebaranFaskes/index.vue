3e167099e1dde4e16c6384788eceda1268522495
akatgelar <akatgelar@gmail.com>
2020-04-11 21:54:39 +0700

    feat: create new map component, cluster, polygon, faskes

---
+++ components/MapV2SebaranFaskes/index.vue
@@ -0,0 +1,326 @@
+<template>
+  <div
+    class="bg-white overflow-hidden rounded-lg shadow-md"
+  >
+    <div class="container-map">
+      <div
+        id="map-wrap-faskes"
+        style="height: 450px; z-index:0; position: relative;"
+      />
+      <div class="filter-layer">
+        <div class="text-right">
+          <button
+            class="btn btn-light"
+            style="background-color: white"
+            @click="backToHome()"
+          >
+            <font-awesome-icon :icon="faHome" />
+            <i class="fas fa-home cc-primary" style="color: black !important;" />
+          </button><br>
+          <button
+            class="btn btn-light mt-2"
+            style="background-color: white"
+            @click="showFilter()"
+          >
+            <font-awesome-icon :icon="faFilter" />
+            <i class="fas fa-filter cc-primary" style="color: black !important;" />
+          </button>
+        </div>
+        <div
+          v-if="isShowFilter"
+          class="filter-data">
+          <li
+            :class="filter.positif_proses?'filter-active':''"
+            @click="setFilter('positif_proses')">
+            <div
+              class="legend-color cluster-positif-proses"
+              style="margin-right: 0.5em;" />Positif - Aktif
+          </li>
+          <li
+            :class="filter.positif_sembuh?'filter-active':''"
+            @click="setFilter('positif_sembuh')">
+            <div
+              class="legend-color cluster-positif-sembuh"
+              style="margin-right: 0.5em;" />Positif - Sembuh
+          </li>
+          <li
+            :class="filter.positif_meninggal?'filter-active':''"
+            @click="setFilter('positif_meninggal')">
+            <div
+              class="legend-color cluster-positif-meninggal"
+              style="margin-right: 0.5em;" />Positif - Meninggal
+          </li>
+          <li
+            :class="filter.pdp_proses?'filter-active':''"
+            @click="setFilter('pdp_proses')">
+            <div
+              class="legend-color cluster-pdp-proses"
+              style="margin-right: 0.5em;" />PDP - Proses
+          </li>
+          <li
+            :class="filter.pdp_selesai?'filter-active':''"
+            @click="setFilter('pdp_selesai')">
+            <div
+              class="legend-color cluster-pdp-selesai"
+              style="margin-right: 0.5em;" />PDP - Selesai
+          </li>
+          <li
+            :class="filter.pdp_meninggal?'filter-active':''"
+            @click="setFilter('pdp_meninggal')">
+            <div
+              class="legend-color cluster-pdp-meninggal"
+              style="margin-right: 0.5em;" />PDP - Meninggal
+          </li>
+          <li
+            :class="filter.odp_proses?'filter-active':''"
+            @click="setFilter('odp_proses')">
+            <div
+              class="legend-color cluster-odp-proses"
+              style="margin-right: 0.5em;" />ODP - Proses
+          </li>
+          <li
+            :class="filter.odp_selesai?'filter-active':''"
+            @click="setFilter('odp_selesai')">
+            <div
+              class="legend-color cluster-odp-selesai"
+              style="margin-right: 0.5em;" />ODP - Selesai
+          </li>
+          <li
+            :class="filter.odp_meninggal?'filter-active':''"
+            @click="setFilter('odp_meninggal')">
+            <div
+              class="legend-color cluster-odp-meninggal"
+              style="margin-right: 0.5em;" />ODP - Meninggal
+          </li>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { faFilter, faHome } from '@fortawesome/free-solid-svg-icons'
+
+export default {
+  name: 'MapV2SebaranFaskes',
+  data () {
+    return {
+      map: '',
+      zoom: 8,
+      faFilter,
+      faHome,
+      isShowFilter: false,
+      filter: {
+        positif_proses: false,
+        positif_meninggal: true,
+        positif_sembuh: true,
+        pdp_proses: false,
+        pdp_selesai: false,
+        pdp_meninggal: false,
+        odp_proses: false,
+        odp_selesai: false,
+        odp_meninggal: false
+      }
+    }
+  },
+  created () {
+    this.setFilter('positif_proses')
+  },
+  methods: {
+    showFilter () {
+      this.isShowFilter = !this.isShowFilter
+    },
+    setFilter (category) {
+      this.filter[category] = !this.filter[category]
+    }
+  },
+  head () {
+    return {
+      link: [
+        { rel: 'stylesheet', href: 'https://unpkg.com/leaflet-geosearch@2.6.0/assets/css/leaflet.css' }
+      ]
+    }
+  }
+}
+</script>
+<style>
+@import "leaflet.markercluster/dist/MarkerCluster.css";
+@import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+
+</style>
+
+<style scoped>
+
+.btn {
+  border-radius: 0.25rem;
+}
+
+.container-map {
+    width:100%;
+    height:100%;
+    position: relative;
+}
+
+.filter-layer {
+    position: absolute;
+    top: 0px;
+    right: 10px;
+    padding-right: 1em;
+    padding-top: 1em;
+}
+
+.filter-layer .btn {
+    font-size: 0.8em;
+    padding: 2px 6px;
+    box-shadow: 0 1px 5px rgba(0,0,0,0.65);
+}
+
+.filter-data {
+    background: #fff;
+    margin-top: 0.5em;
+    padding: 0.6em;
+    border-radius: 0.6em;
+    box-shadow: 0 1px 5px rgba(0,0,0,0.65);
+}
+
+.filter-layer li {
+    list-style: none;
+    opacity: 0.4;
+    padding-bottom: 0.2em;
+}
+
+.filter-layer li:hover {
+    cursor: pointer;
+}
+
+.filter-active {
+    opacity: 1 !important;
+}
+
+.filter-data {
+    background: #fff;
+    color: black;
+    margin-top: 0.5em;
+    padding: 0.6em;
+    border-radius: 0.6em;
+    box-shadow: 0 1px 5px rgba(0,0,0,0.65);
+}
+
+.legend-color {
+    width: 1em;
+    height: 1em;
+    float: left;
+    border-radius: 10px;
+    margin-top: 4px;
+}
+
+.cluster {
+    border-radius: 50%;
+    text-align: center;
+    color: white;
+    font-weight: 700;
+    font-family: monospace;
+    height: 10px;
+    width: 10px;
+}
+
+.cluster-odp-proses {
+  /* box-shadow: 0 0 5px 0 rgb(45, 156, 219, 0.9); */
+  border: 2px solid rgb(45, 156, 219, 0.9);
+  background: rgb(45, 156, 219, 0.9);
+}
+
+.cluster-odp-selesai {
+  /* box-shadow: 0 0 5px 0 rgb(45, 156, 219, 0.9); */
+  border: 2px solid rgb(45, 156, 219, 0.9);
+  background: rgba(196, 195, 195, 0.9);
+}
+
+.cluster-odp-meninggal {
+  /* box-shadow: 0 0 5px 0 rgb(45, 156, 219, 0.9); */
+  border: 2px solid rgb(45, 156, 219, 0.9);
+  background: rgb(165,18,18, 0.9);
+}
+
+.cluster-pdp-proses {
+  /* box-shadow: 0 0 5px 0 rgb(242, 201, 76, 0.9); */
+  border: 2px solid rgb(242, 201, 76, 0.9);
+  background: rgb(242, 201, 76, 0.9);
+}
+
+.cluster-pdp-selesai {
+  /* box-shadow: 0 0 5px 0 rgb(242, 201, 76, 0.9); */
+  border: 2px solid rgb(242, 201, 76, 0.9);
+  background: rgba(196, 195, 195, 0.9);
+}
+
+.cluster-pdp-meninggal {
+  /* box-shadow: 0 0 5px 0 rgb(242, 201, 76, 0.9); */
+  border: 2px solid rgb(242, 201, 76, 0.9);
+  background: rgb(165,18,18, 0.9);
+}
+
+.cluster-positif-proses {
+  /* box-shadow: 0 0 5px 0 rgb(235, 87, 87, 0.9); */
+  border: 2px solid rgb(235, 87, 87, 0.9);
+  background: rgb(235, 87, 87, 0.9);
+}
+
+.cluster-positif-sembuh {
+  /* box-shadow: 0 0 5px 0 rgb(235, 87, 87, 0.9); */
+  border: 2px solid rgb(235, 87, 87, 0.9);
+  background: rgb(39, 174, 96, 0.9);
+}
+
+.cluster-positif-meninggal {
+  /* box-shadow: 0 0 5px 0 rgb(235, 87, 87, 0.9); */
+  border: 2px solid rgb(235, 87, 87, 0.9);
+  background: rgb(165,18,18, 0.9);
+}
+
+.digits-0 {
+  height: 17px;
+  width: 17px;
+  margin-top: -14px;
+  margin-left: -14px;
+}
+
+.digits-1 {
+  font-size: 14px;
+  height: 28px;
+  width: 28px;
+  line-height: 28px;
+  margin-top: -14px;
+  margin-left: -14px;
+}
+
+.digits-2 {
+  font-size: 16px;
+  height: 34px;
+  width: 34px;
+  line-height: 35px;
+  margin-top: -17px;
+  margin-left: -17px;
+}
+
+.digits-3 {
+  font-size: 18px;
+  height: 48px;
+  width: 47px;
+  line-height: 47px;
+  /* border-width: 3px; */
+  margin-top: -24px;
+  margin-left: -24px;
+}
+
+.digits-4 {
+  font-size: 18px;
+  height: 58px;
+  width: 58px;
+  line-height: 57px;
+  /* border-width: 4px; */
+  margin-top: -29px;
+  margin-left: -29px;
+}
+</style>
