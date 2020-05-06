71e7a387a09dccb5efb700825332b97defb61483
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-05-06 12:01:04 +0700

    fix: fetch data with no intermediary

--- api/donation.js
+++ api/donation.js
@@ -1,20 +1,42 @@
 import axios from 'axios'
 
 const WMSApi = axios.create({
-  baseURL: process.env.WMS_BASE_URL
+  baseURL: process.env.WMS_BASE_URL,
+  headers: {
+    'api-key': process.env.WMS_API_KEY
+  }
 })
 
-export function getCollectedDonations () {
-  return WMSApi.get('material', {
-    headers: {
-      'api-key': process.env.WMS_API_KEY
-    }
-  })
+export {
+  WMSApi
+}
+
+export async function getCollectedDonations (config = {}) {
+  const data = await WMSApi.get('master/material', config)
     .then((r) => {
-      try {
-        return r.data.msg
-      } catch (e) {
-        return []
-      }
+      return r.data.data
     })
+  const total = await WMSApi.get('master/material', {
+    params: {
+      count: true
+    }
+  }).then((r) => {
+    return r.data.data.count
+  })
+  return {
+    total,
+    data
+  }
+}
+
+export async function getLogistics (config = {}) {
+  const data = await WMSApi.get('api/logistik', config)
+    .then((r) => {
+      return r.data.data
+    }).catch((e) => {
+      return []
+    })
+  return {
+    data
+  }
 }
