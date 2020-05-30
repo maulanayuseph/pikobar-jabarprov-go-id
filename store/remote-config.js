9fb299dce4f6c18658841556adeacedd10b7cf92
Ihsan <ihsan@varnion.com>
2020-05-30 18:04:27 +0700

    fix: whatspp link informasi donasi

--- store/remote-config.js
+++ store/remote-config.js
@@ -42,7 +42,8 @@ export const actions = {
             enableDownload,
             downloadAppURL,
             selfDiagnoseURL,
-            announcement: JSON.parse(announcement)
+            announcement
+            // announcement: JSON.parse(announcement)
           })
         })
     }
