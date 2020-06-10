046609f3f7ad5b24d9d5e8345584b79dfa6b4f83
akatgelar <akatgelar@gmail.com>
2020-06-10 17:43:47 +0700

    feat: store all api dashboard

---
+++ store/data-sebaran-jabar.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export const state = () => ({
+  items: null
+})
+
+export const mutations = {
+  setItems (state, items) {
+    state.items = items
+  }
+}
+
+export const actions = {
+  async getItems ({ commit }, options) {
+    const { data } = await axios.get('https://covid19-public.digitalservice.id/api/v1/sebaran/jabar')
+    commit('setItems', data.data.content)
+  }
+}
+
+export const getters = {
+  itemsMap (state) {
+    return state.items
+  }
+}
