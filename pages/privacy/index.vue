0a778d2be5eb2d8076c8fdd5ba166db5b04c28a1
adrianpdm <adrian.padmodihardjo@gmail.com>
2020-07-07 16:08:41 +0700

    PrivacyPage: set page layout and content

---
+++ pages/privacy/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-16 m-4 md:m-8">
+    <header class="text-center">
+      <h1 class="text-2xl font-bold">
+        {{ TAC.title }}
+      </h1>
+      <span class="opacity-75">
+        {{ TAC.date }}
+      </span>
+    </header>
+    <div class="mt-10 html-content">
+      <strong>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <p v-html="TAC.agreement" />
+      </strong>
+      <!-- eslint-disable-next-line vue/no-v-html -->
+      <p v-html="TAC.description" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    TAC () {
+      return this.$store.state['remote-config'].config.termsAndCondition || {}
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
