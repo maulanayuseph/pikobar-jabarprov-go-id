ce4a2fa452e1e5c5ef4a9adaaa483375b8472b19
akatgelar <akatgelar@gmail.com>
2020-07-16 16:25:21 +0700

    fix: style responsive

--- components/_pages/index/StatisticLoader.vue
+++ components/_pages/index/StatisticLoader.vue
@@ -41,28 +41,32 @@ export default {
             <rect x={0} y={64} rx={8} ry={6} width="20%" height="16"></rect>
           </ContentLoader></div>
         <div style={`display: ${context.props.isPending ? 'none' : 'block'}`}>
-          <div style="display: flex;">
-            <h3 class="p-5" style="font-size: 23px !important;">
-              <b>
-                { context.props.label }
-              </b>
-            </h3>
-            <div class="mr-4 md:mb-0" style="display: flex; margin: auto;">
-              <span
-                class="text-2xl font-bold inline-block"
-                style="color: #2DAC55; min-width: 2ch;">
-                { formatNumber(context.props.total) }
-              </span>
-              <p
-                class="mt-2 ml-2 text-base whitespace-no-wrap"
-                style="color: #8A8A8A;">
-                { context.props.totalLabel }
-              </p>
+          <div class="flex flex-row flex-wrap">
+            <div class="w-full lg:w-2/3">
+              <h3 class="p-5" style="font-size: 23px !important;">
+                <b>
+                  { context.props.label }
+                </b>
+              </h3>
+            </div>
+            <div class="pl-5 md:mb-0 w-full lg:w-1/3 lg:pt-5">
+              <div style="display: flex; margin: auto;">
+                <span
+                  class="text-2xl font-bold inline-block"
+                  style="color: #2DAC55; min-width: 2ch;">
+                  { formatNumber(context.props.total) }
+                </span>
+                <p
+                  class="mt-2 ml-2 text-base whitespace-no-wrap"
+                  style="color: #8A8A8A;">
+                  { context.props.totalLabel }
+                </p>
+              </div>
             </div>
           </div>
           <hr />
           <div class="p-5 flex flex-col md:flex-row md:justify-start md:items-end leading-none">
-            <div class="mb-4 md:mb-0 w-1/3">
+            <div class="mb-4 md:mb-0 lg:w-1/3">
               <span
                 class="text-4xl font-bold inline-block"
                 style="color: #2DAC55; min-width: 2ch;">
@@ -71,7 +75,7 @@ export default {
               <span
                 class="text-sm font-bold ml-2"
                 style="color: #000000;">
-                { `(${formatNumber(context.props.processedPercentage)}% )` }
+                { `(${formatNumber(context.props.processedPercentage)} % )` }
               </span>
               <p
                 class="mt-2 text-base whitespace-no-wrap"
@@ -79,7 +83,7 @@ export default {
                 { context.props.processedLabel }
               </p>
             </div>
-            <div class="mb-4 md:mb-0 w-1/3">
+            <div class="mb-4 md:mb-0 lg:w-1/3">
               <span
                 class="text-2xl font-bold inline-block"
                 style="color: #2DAC55; min-width: 2ch;">
@@ -88,7 +92,7 @@ export default {
               <span
                 class="text-sm font-bold ml-2"
                 style="color: #000000;">
-                { `(${formatNumber(context.props.finishedPercentage)}%)` }
+                { `(${formatNumber(context.props.finishedPercentage)} %)` }
               </span>
               <p
                 class="mt-2 text-base whitespace-no-wrap "
@@ -96,7 +100,7 @@ export default {
                 { context.props.finishedLabel }
               </p>
             </div>
-            <div class="mb-4 md:mb-0 w-1/3">
+            <div class="mb-4 md:mb-0 lg:w-1/3">
               <span
                 class="text-2xl font-bold inline-block"
                 style="color: #2DAC55; min-width: 2ch;">
@@ -105,7 +109,7 @@ export default {
               <span
                 class="text-sm font-bold ml-2"
                 style="color: #000000;">
-                { `(${formatNumber(context.props.diedPercentage)}%)` }
+                { `(${formatNumber(context.props.diedPercentage)} %)` }
               </span>
               <p
                 class="mt-2 text-base whitespace-no-wrap "
