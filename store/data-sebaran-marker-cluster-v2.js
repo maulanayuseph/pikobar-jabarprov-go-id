253745eb8ef4a2842178f3f5328707cee36d7a69
akatgelar <akatgelar@gmail.com>
2020-08-11 11:58:12 +0700

    feat: new api v2 - istilah baru

---
+++ store/data-sebaran-marker-cluster-v2.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+export const state = () => ({
+  items: null,
+  item: null,
+  isLoading: false
+})
+
+export const mutations = {
+  setItems (state, items) {
+    state.items = items
+  },
+  setItem (state, item) {
+    state.item = item
+  },
+  setIsLoading (state, item) {
+    state.isLoading = item
+  }
+}
+
+export const actions = {
+  async getItems ({ commit }, options) {
+    const { data } = await axios.get('https://dashboard-pikobar-api.digitalservice.id/v2/sebaran/marker_cluster', {
+      headers: {
+        'api-key': process.env.DASHBOARD_API_KEY
+      }
+    })
+    commit('setItems', data.data)
+  },
+
+  async getItem ({ commit }, query, options) {
+    commit('setIsLoading', true)
+    const { data } = await axios.get('https://dashboard-pikobar-api.digitalservice.id/v2/sebaran/marker_cluster?' + query, {
+      headers: {
+        'api-key': process.env.DASHBOARD_API_KEY
+      }
+    })
+    commit('setItem', data.data)
+    commit('setIsLoading', false)
+  }
+}
+
+export const getters = {
+  itemsMap (state) {
+    return state.items
+  },
+  itemMap (state) {
+    return state.item
+  },
+  isLoading (state) {
+    return state.isLoading
+  }
+}
---
+++ store/data-sebaran-pertumbuhan-v2.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+export const state = () => ({
+  items: null,
+  item: null,
+  isLoading: false
+})
+
+export const mutations = {
+  setItems (state, items) {
+    state.items = items
+  },
+  setItem (state, item) {
+    state.item = item
+  },
+  setIsLoading (state, item) {
+    state.isLoading = item
+  },
+  setMetaData (state, item) {
+    state.metadata = item
+  }
+}
+
+export const actions = {
+  async getItems ({ commit }, options) {
+    const { data } = await axios.get('https://dashboard-pikobar-api.digitalservice.id/v2/sebaran/pertumbuhan', {
+      headers: {
+        'api-key': process.env.DASHBOARD_API_KEY
+      }
+    })
+    commit('setItems', data.data)
+  },
+
+  async getItem ({ commit }, query, options) {
+    commit('setIsLoading', true)
+    const { data } = await axios.get('https://dashboard-pikobar-api.digitalservice.id/v2/sebaran/pertumbuhan?' + query, {
+      headers: {
+        'api-key': process.env.DASHBOARD_API_KEY
+      }
+    })
+    commit('setItem', data.data)
+    commit('setIsLoading', false)
+    commit('setMetaData', data.metadata)
+  }
+}
+
+export const getters = {
+  itemsMap (state) {
+    return state.items
+  },
+  itemMap (state) {
+    return state.item
+  },
+  isLoading (state) {
+    return state.isLoading
+  },
+  metadata (state) {
+    return state.metadata
+  }
+}
