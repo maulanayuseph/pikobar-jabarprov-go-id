10643c6663cac2c59077a9f4cfc58c4cbed59d03
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-04 00:34:07 +0700

    define content using json

---
+++ components/_pages/index/IsolasiMandiri/anjuran-tempat-perawatan.js
@@ -0,0 +1,85 @@
+export default [
+  {
+    title: 'Tanpa Gejala',
+    sections: [
+      {
+        title: 'Gejala',
+        body: [
+          'Frekuensi napas: 12-20 kali per menit',
+          'Saturasi: >= 95%'
+        ]
+      },
+      {
+        title: 'Tempat perawatan',
+        body: [
+          'Isoman di rumah',
+          'Fasilitas isolasi pemerintah'
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Ringan',
+    sections: [
+      {
+        title: 'Gejala',
+        body: [
+          'Demam, batuk, sakit kepala, mual, kehilangan indra penciuman, dan pengecapan',
+          'Frekuensi napas: 12-20 kali per menit',
+          'Saturasi: >= 95%'
+        ]
+      },
+      {
+        title: 'Tempat perawatan',
+        body: [
+          'Fasilitas isolasi pemerintah',
+          'Isoman di rumah bagi yang memenuhi syarat'
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Sedang',
+    sections: [
+      {
+        title: 'Gejala',
+        body: [
+          'Hampir sama dengan gejala pasien ringan',
+          '<b>namun dengan</b><br>Frekuensi napas: 20-30 kali per menit',
+          'Saturasi: < 95%',
+          'Sesak napas tanpa distress pernapasan'
+        ]
+      },
+      {
+        title: 'Tempat perawatan',
+        body: [
+          'RS Lapangan',
+          'RS Darurat Covid-19',
+          'RS Non Rujukan',
+          'RS Rujukan'
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Berat-Kritis',
+    sections: [
+      {
+        title: 'Gejala',
+        body: [
+          'Hampir sama dengan gejala pasien sedang',
+          '<b>namun dengan</b><br>Frekuensi napas: >30 kali per menit',
+          'Saturasi: < 95%',
+          'Sesak napas dengan distress pernapasan',
+          '<b>Kondisi kritis: </b> gagal napas, sepsis, syok sepsis, dan <em>multiorgan failure</em>'
+        ]
+      },
+      {
+        title: 'Tempat perawatan',
+        body: [
+          'HCU/ICU RS Rujukan'
+        ]
+      }
+    ]
+  }
+]
