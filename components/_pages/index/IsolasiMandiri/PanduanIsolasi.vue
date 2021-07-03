308f885f543c4431970c9fef8a7f001cb3a2693a
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-04 00:35:07 +0700

    create panduan isolasi component

---
+++ components/_pages/index/IsolasiMandiri/PanduanIsolasi.vue
@@ -0,0 +1,68 @@
+<template>
+  <p class="panduan-isolasi">
+    Hal pertama yang perlu Wargi lakukan ketika mengetahui terkonfirmasi positif
+    adalah dengan melaporkan diri ke puskesmas/fasilitas pelayanan kesehatan (fasyankes)
+    terdekat untuk pemantauan kesehatan. Tak hanya untuk mendapatkan pemeriksaan dini
+    oleh tenaga medis, pelaporan ini juga diperlukan untuk proses pelacakan kontak erat.
+    <br>
+    <br>
+    Ketika Wargi terkonfirmasi tanpa gejala atau dengan gejala ringan, maka yang perlu
+    Wargi lakukan adalah melakukan isolasi mandiri. Apa yang perlu Wargi siapkan untuk isoman?
+    <br>
+    <br>
+    <ol>
+      <li>
+        <strong>Tinggal di rumah</strong>, kecuali untuk kepentingan medis
+      </li>
+      <li>
+        <strong>Gunakan kamar tidur terpisah</strong> dengan anggota keluarga lainnya,
+        dan jika memungkinkan dengan kamar mandi terpisah.
+      </li>
+      <li>
+        <strong>Terapkan protokol kesehatan</strong> (gunakan masker, mencuci tangan, menjaga jarak)
+      </li>
+      <li>
+        <strong>Lakukan pengukuran suhu harian dan observasi gejala klinis</strong> seperti
+        batuk atau kesulitan bernapas.
+      </li>
+      <li>
+        <strong>Hindari pemakaian bersama peralatan makan, mandi, dan linen/sprei.</strong>
+      </li>
+      <li>
+        <strong>Terapkan Perilaku Hidup Bersih dan Sehat (PHBS)</strong>
+        : konsumsi makanan bergizi, mencuci tangan dengan sabun dan air mengalir, dan lakukan
+        etika batuk/bersin.
+      </li>
+      <li>
+        <strong>Berjemur di bawah sinar matahari setiap pagi.</strong>
+      </li>
+      <li>
+        <strong>Jaga kebersihan</strong> rumah dengan disinfektan.
+      </li>
+      <li>
+        <strong>Segera hubungi fasyankes terdekat jika terjadi perburukan gejala</strong> untuk
+        perawatan lebih lanjut.
+      </li>
+    </ol>
+    <br>
+    Isolasi mandiri dilakukan selama ±10 hari atau hingga pasien memenuhi kriteria sembuh
+    Covid-19. Isolasi di rumah bagi pasien tanpa gejala maupun yang bergejala ringan, bisa menjadi
+    hal yang berat bagi sebagian orang. Namun, cara ini efektif untuk melindungi Wargi, keluarga,
+    serta orang lain.
+  </p>
+</template>
+
+<style lang="scss">
+.panduan-isolasi {
+  @apply text-gray-800;
+
+  ol {
+    @apply list-decimal list-outside;
+
+    li {
+      @apply ml-4 pl-4;
+    }
+  }
+
+}
+</style>
