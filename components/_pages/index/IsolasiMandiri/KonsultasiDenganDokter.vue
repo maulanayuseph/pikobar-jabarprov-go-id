ee6ad08e37498984e5bd6b8f3d319006ad6c4539
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-04 00:35:19 +0700

    create konsultasi dokter component

---
+++ components/_pages/index/IsolasiMandiri/KonsultasiDenganDokter.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>
+    Jika selama menjalani isolasi mandiri Wargi memerlukan pelayanan kesehatan
+    dan membutuhkan pemeriksaan kesehatan lebih lanjut, Pemerintah Daerah Provinsi Jawa Barat
+    menawarkan pelayanan dokter telekonsultasi melalui
+    <a
+      class="text-green-600 underline font-bold"
+      :href="whatsappBacklink"
+      target="_blank"
+      @click.prevent="onClick"
+    >
+      link ini.
+    </a>
+  </p>
+</template>
+
+<script>
+import { konsultasiDokter } from './backlinks'
+import { TAP_KONSULTASI_DOKTER } from './events'
+import { analytics } from '~/lib/firebase'
+export default {
+  data () {
+    return {
+      whatsappBacklink: konsultasiDokter
+    }
+  },
+  methods: {
+    onClick () {
+      if (analytics) {
+        analytics.logEvent(TAP_KONSULTASI_DOKTER)
+      }
+      window.open(this.whatsappBacklink, '_blank')
+      return true
+    }
+  }
+}
+</script>
