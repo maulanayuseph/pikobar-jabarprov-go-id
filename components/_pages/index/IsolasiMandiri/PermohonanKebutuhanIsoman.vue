3f4372b8c3da303ffaef96f781cfbeef1fbd9d5c
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-04 00:34:57 +0700

    create permohonan kebutuhan isoman component

---
+++ components/_pages/index/IsolasiMandiri/PermohonanKebutuhanIsoman.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>
+    Bagi Wargi Jabar yang kesulitan mendapatkan akses obat dan multivitamin selama
+    melakukan isolasi mandiri di rumah, melalui rekomendasi dokter telekonsultasi kami,
+    Wargi bisa mengajukan permohonan kebutuhan obat-obatan Panduan
+    <a
+      class="text-green-600 underline font-bold"
+      :href="formLink"
+      target="_blank"
+      @click.prevent="onClick"
+    >
+      link ini.
+    </a>
+  </p>
+</template>
+
+<script>
+import { permohonanKebutuhan } from './backlinks'
+import { TAP_PERMOHONAN_ISOMAN } from './events'
+import { analytics } from '~/lib/firebase'
+export default {
+  data () {
+    return {
+      formLink: permohonanKebutuhan
+    }
+  },
+  methods: {
+    onClick () {
+      if (analytics) {
+        analytics.logEvent(TAP_PERMOHONAN_ISOMAN)
+      }
+      window.open(this.formLink, '_blank')
+      return true
+    }
+  }
+}
+</script>
