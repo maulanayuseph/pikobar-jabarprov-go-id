7f3beec9b246fa61d549cca5a5daf137ceafd4ff
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-07-09 14:41:11 +0700

    add page oxygen

---
+++ pages/oxygen/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="">
+    <div class="bg-brand-green-dark">
+      <div class="container mx-auto p-8 lg:p-16 text-white">
+        <h2 class="text-2xl font-bold leading-tight">
+          Daftar Agen Oxygen di Jawa Barat
+        </h2>
+        <p class="mb-6 mt-2 text-base opacity-75 leading-tight">
+          Informasi dan kontak agen oksigen (isi ulang dan tabung oksigen) di Jawa Barat
+        </p>
+      </div>
+    </div>
+    <br>
+    <div class="container mx-auto">
+      <div class="m-4 md:m-8 p-5 md:p-8 rounded-lg bg-white shadow-md">
+        <h2 class="text-2xl font-bold leading-tight mb-6">
+          Konten Edukasi Tentang Penggunaan Oksigen
+        </h2>
+        <ExpandableContent open v-for="(item, i) in infoItems" :key="i">
+          <template #title>
+            {{ item.title }}
+          </template>
+          <div v-html="item.content" />
+        </ExpandableContent>
+      </div>
+      <OxygenAccordion />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { analytics } from '~/lib/firebase'
+import ExpandableContent from '~/components/_pages/index/IsolasiMandiri/ExpandableContent'
+export default {
+  components: {
+    OxygenAccordion: () => import('~/components/OxygenAccordion'),
+    ExpandableContent
+  },
+  computed: {
+    ...mapState('oxygen', [
+      'infoItems'
+    ])
+  },
+  mounted () {
+    this.$store.dispatch('oxygen/getItems')
+    this.$store.dispatch('oxygen/getInfoItems')
+    this.$nextTick(() => {
+      if (process.browser) {
+        analytics.logEvent('contacts_view')
+      }
+    })
+  },
+  head () {
+    const title = 'Kontak - Pikobar [Pusat Informasi dan Koordinasi COVID-19 Jawa Barat]'
+    return {
+      title,
+      meta: [
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: title
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'article'
+        }
+      ]
+    }
+  }
+}
+</script>
