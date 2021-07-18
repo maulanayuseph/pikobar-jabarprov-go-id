980ae359f94269181c6293c97ef977c8d3a4e7f1
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-07-18 07:31:00 +0700

    add empty tracking result component

---
+++ components/_pages/index/Tracking/EmptyResult.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mx-auto">
+    <div class=" md:m-8 md:p-8 result-card">
+      <img src="~assets/tracking-not-found.png" width="350px">
+      <span class="text-2xl font-bold">
+        Data Permohonan Tidak Ditemukan
+      </span>
+      <span>
+        Maaf, Kami tidak dapat menemukan Data Permohonan yang Anda cari.
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      halo: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.result-card {
+  @apply flex flex-col p-5 m-4
+  rounded-lg bg-white shadow-md
+  items-center
+}
+</style>
