f13e9f55a60c6a56830ab0a83101f61242878f51
naufalihsank <56071566+naufalihsank@users.noreply.github.com>
2021-07-18 07:33:25 +0700

    update env in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,9 @@ export default {
     WMS_API_KEY: process.env.WMS_API_KEY,
     WMS_BASE_URL: process.env.WMS_BASE_URL,
     RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
-    DASHBOARD_API_KEY: process.env.DASHBOARD_API_KEY
+    DASHBOARD_API_KEY: process.env.DASHBOARD_API_KEY,
+    GOOGLE_RECAPTCHA_KEY: process.env.GOOGLE_RECAPTCHA_KEY,
+    ISOMAN_API_BASEURL: process.env.ISOMAN_API_BASEURL
   },
   router: {
     prefetchLinks: false
