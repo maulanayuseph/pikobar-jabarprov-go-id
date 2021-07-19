0e114f7535722b51df1efdacc6097bd85270a6d6
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-19 16:08:46 +0700

    add analytic event for oxygen form action card

--- components/_pages/index/IsolasiMandiri/events.js
+++ components/_pages/index/IsolasiMandiri/events.js
@@ -1,7 +1,11 @@
 const TAP_PERMOHONAN_ISOMAN = 'tapped_permohonan_isoman'
 const TAP_KONSULTASI_DOKTER = 'tapped_konsultasi_dokter'
+const TAP_PEMINJAM_OKSIGEN = 'tapped_peminjam_oksigen'
+const TAP_PEMBERI_OKSIGEN = 'tapped_pemberi_oksigen'
 
 export {
   TAP_KONSULTASI_DOKTER,
-  TAP_PERMOHONAN_ISOMAN
+  TAP_PERMOHONAN_ISOMAN,
+  TAP_PEMINJAM_OKSIGEN,
+  TAP_PEMBERI_OKSIGEN
 }
