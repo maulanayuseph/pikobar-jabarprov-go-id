83bcc01a5df0fd8f4814499d72cc28580d963da7
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-07-19 16:11:56 +0700

    fetch oxygen forms backlinks on mounted

--- components/_pages/index/IsolasiMandiri/index.vue
+++ components/_pages/index/IsolasiMandiri/index.vue
@@ -64,20 +64,22 @@
         :backlink="permohonanKebutuhan"
       />
       <ActionCard
+        :class="asyncCardClasses"
         title="Saya Butuh Tabung Oksigen"
         body="Lihat daftar penyedia tabung oksigen di sekitar Anda"
         prompt="Cek di Sini"
-        :event="konsultasiDokterEvent"
-        :image="konsultasiDokterImage"
-        :backlink="konsultasiDokter"
+        :event="peminjamOksigenEvent"
+        :image="peminjamOksigenImage"
+        :backlink="peminjamOksigenJotform"
       />
       <ActionCard
+        :class="asyncCardClasses"
         title="Saya Punya Tabung Oksigen"
         body="Lengkapi formulir untuk meminjamkan tabung oksigen bagi warga yang membutuhkan"
         prompt="Isi Formulir"
-        :event="permohonanKebutuhanEvent"
-        :image="permohonanKebutuhanImage"
-        :backlink="permohonanKebutuhan"
+        :event="pemberiOksigenEvent"
+        :image="pemberiOksigenImage"
+        :backlink="pemberiOksigenJotform"
       />
     </div>
   </div>
@@ -96,10 +98,14 @@ import ActionCard from './ActionCard'
 import { konsultasiDokter, permohonanKebutuhan } from './backlinks'
 import {
   TAP_KONSULTASI_DOKTER as konsultasiDokterEvent,
-  TAP_PERMOHONAN_ISOMAN as permohonanKebutuhanEvent
+  TAP_PERMOHONAN_ISOMAN as permohonanKebutuhanEvent,
+  TAP_PEMINJAM_OKSIGEN as peminjamOksigenEvent,
+  TAP_PEMBERI_OKSIGEN as pemberiOksigenEvent
 } from './events'
 import permohonanKebutuhanImage from '~/assets/illustrations/permohonan-kebutuhan-isoman.png'
 import konsultasiDokterImage from '~/assets/illustrations/konsultasi-dokter.png'
+import peminjamOksigenImage from '~/assets/illustrations/peminjam-oksigen.png'
+import pemberiOksigenImage from '~/assets/illustrations/pemberi-oksigen.png'
 
 export default {
   components: {
@@ -120,7 +126,48 @@ export default {
       konsultasiDokterEvent,
       permohonanKebutuhan,
       permohonanKebutuhanImage,
-      permohonanKebutuhanEvent
+      permohonanKebutuhanEvent,
+      peminjamOksigenImage,
+      peminjamOksigenEvent,
+      peminjamOksigenJotform: null,
+      pemberiOksigenImage,
+      pemberiOksigenEvent,
+      pemberiOksigenJotform: null,
+      isBacklinkLoading: true,
+      isBacklinkError: false
+    }
+  },
+  computed: {
+    asyncCardClasses () {
+      if (this.isBacklinkLoading) {
+        return 'action-card--loading'
+      }
+      if (this.isBacklinkError) {
+        return 'action-card--error'
+      }
+      return ''
+    }
+  },
+  mounted () {
+    this.getBacklinksFromRemoteConfig()
+  },
+  methods: {
+    async getBacklinksFromRemoteConfig () {
+      this.isBacklinkLoading = true
+      this.isBacklinkError = false
+      this.peminjamOksigenJotform = null
+      this.pemberiOksigenJotform = null
+      try {
+        const backlinks = await this.$store.dispatch('oxygen/getFormBacklinks')
+        if (backlinks) {
+          this.peminjamOksigenJotform = backlinks.form_request
+          this.pemberiOksigenJotform = backlinks.form_provide
+        }
+      } catch (e) {
+        this.isBacklinkError = true
+      } finally {
+        this.isBacklinkLoading = false
+      }
     }
   }
 }
