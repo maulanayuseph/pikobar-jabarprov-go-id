b7288a1169bafe0b286c7be25748ca610311cca4
Maulana Yuseph <maulanayuseph@gmail.com>
2021-07-27 07:51:53 +0700

    add components testimonial

--- pages/isoman/index.vue
+++ pages/isoman/index.vue
@@ -24,14 +24,21 @@
         <IsolasiMandiri />
       </div>
     </section>
+    <section class="container mx-auto">
+      <div class="m-4 md:m-8 p-5 md:p-8 rounded-lg bg-white shadow-md">
+        <Testimonial />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import IsolasiMandiri from '../../components/_pages/index/IsolasiMandiri'
+import Testimonial from '../../components/_pages/testimonial'
 export default {
   components: {
-    IsolasiMandiri
+    IsolasiMandiri,
+    Testimonial
   }
 }
 </script>
