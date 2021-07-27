09001c11fb90021af663526487bead1fb10f6d13
Maulana Yuseph <maulanayuseph@gmail.com>
2021-07-27 13:27:31 +0700

    revisi naming keys of object

--- components/_pages/testimonial/testimoni.js
+++ components/_pages/testimonial/testimoni.js
@@ -1,43 +1,43 @@
 export default [
   {
-    Name: 'Febby Wulandari',
-    Address: 'Jl Rawa Silam, Jabar',
-    Testimoni: 'Sangat membantu sekali bagi warga yang kurang mampu membeli vitamin dan obat-obatan.'
+    name: 'Febby Wulandari',
+    address: 'Jl Rawa Silam, Jabar',
+    message: 'Sangat membantu sekali bagi warga yang kurang mampu membeli vitamin dan obat-obatan.'
   }, {
-    Name: 'Rinsa K',
-    Address: 'Vila Indah Permai, Bekasi',
-    Testimoni: 'Terima kasih Pemerintah Jabar, obatnya sampai dirumah saya dengan keadaan bersih dan tidak rusak.'
+    name: 'Rinsa K',
+    address: 'Vila Indah Permai, Bekasi',
+    message: 'Terima kasih Pemerintah Jabar, obatnya sampai dirumah saya dengan keadaan bersih dan tidak rusak.'
   }, {
-    Name: 'Juan Moses Wandy',
-    Address: 'Gg. Kembang, Bogor Utara',
-    Testimoni: 'Terimakasih Pemerintah Jabar dan jajaran Tim Pikobar, anak kami yang isoman berusia 5 tahun sudah mendapatkan vitamin dari program Pikobar ini.'
+    name: 'Juan Moses Wandy',
+    address: 'Gg. Kembang, Bogor Utara',
+    message: 'Terimakasih Pemerintah Jabar dan jajaran Tim Pikobar, anak kami yang isoman berusia 5 tahun sudah mendapatkan vitamin dari program Pikobar ini.'
   }, {
-    Name: 'Novie',
-    Address: 'Bekasi Utara',
-    Testimoni: 'Saya berterima kasih atas obat/ vitamin yang sudah dikirimkan karena sangat membantu dalam proses pemulihan saya dari Virus Corona. Semoga banyak warga yang juga dapat ditolong.'
+    name: 'Novie',
+    address: 'Bekasi Utara',
+    message: 'Saya berterima kasih atas obat/ vitamin yang sudah dikirimkan karena sangat membantu dalam proses pemulihan saya dari Virus Corona. Semoga banyak warga yang juga dapat ditolong.'
   }, {
-    Name: 'Dinda',
-    Address: 'Jl. Lanud Suryadarma, Kab. Subang',
-    Testimoni: 'Alhamdulillah, dapat vitamin gratis dari Satgas Covid-19 Jawa Barat. Tinggal isi form, tunggu vitamin dateng dianter sama kurir.'
+    name: 'Dinda',
+    address: 'Jl. Lanud Suryadarma, Kab. Subang',
+    message: 'Alhamdulillah, dapat vitamin gratis dari Satgas Covid-19 Jawa Barat. Tinggal isi form, tunggu vitamin dateng dianter sama kurir.'
   }, {
-    Name: 'Gusson Satria',
-    Address: 'Desa Cimacan, Kab. Cianjur',
-    Testimoni: 'Kaleresan Abdi masih Isolasi Mandiri, stok Vitamin seep alhamdulillah kengeng kintunan. Hatur Nuhun'
+    name: 'Gusson Satria',
+    address: 'Desa Cimacan, Kab. Cianjur',
+    message: 'Kaleresan Abdi masih Isolasi Mandiri, stok Vitamin seep alhamdulillah kengeng kintunan. Hatur Nuhun'
   }, {
-    Name: 'Kartika',
-    Address: 'Cibabat, Cimahi Utara',
-    Testimoni: 'Terima kasih, program obat/vitamin gratis untuk isoman ternyata benar adanya & sangat membantu dengan adanya perhatian pemerintah.'
+    name: 'Kartika',
+    address: 'Cibabat, Cimahi Utara',
+    message: 'Terima kasih, program obat/vitamin gratis untuk isoman ternyata benar adanya & sangat membantu dengan adanya perhatian pemerintah.'
   }, {
-    Name: 'Rachmita',
-    Address: 'Sumur Bandung, Depok',
-    Testimoni: 'Akhirnya setelah seminggu nyari obat Covid-19, dapet juga, senang rasanya. Hatur nuhun Pikobar!'
+    name: 'Rachmita',
+    address: 'Sumur Bandung, Depok',
+    message: 'Akhirnya setelah seminggu nyari obat Covid-19, dapet juga, senang rasanya. Hatur nuhun Pikobar!'
   }, {
-    Name: 'Elma Anugrah',
-    Address: 'Margahayu, Bekasi Timur',
-    Testimoni: 'Dengan adanya Program Bantuan Obat/Multivitamin, kami bahagia atas perhatian Pak Gubernur beserta jajarannya.'
+    name: 'Elma Anugrah',
+    address: 'Margahayu, Bekasi Timur',
+    message: 'Dengan adanya Program Bantuan Obat/Multivitamin, kami bahagia atas perhatian Pak Gubernur beserta jajarannya.'
   }, {
-    Name: 'Meiza A.',
-    Address: 'Depok, Edelweiss',
-    Testimoni: 'Terima Kasih Kepada Pemerintah Jawa Barat atas paket obat/vitamin. Perhatian besar terhadap warganya yang terpapar virus Covid-19'
+    name: 'Meiza A.',
+    address: 'Depok, Edelweiss',
+    message: 'Terima Kasih Kepada Pemerintah Jawa Barat atas paket obat/vitamin. Perhatian besar terhadap warganya yang terpapar virus Covid-19'
   }
 ]
